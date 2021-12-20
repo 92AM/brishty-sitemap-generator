1837d2bfc4b23c8f79dbb608eeec444629e55c12
Arka Mitra <arka2k8@gmail.com>
2021-12-20 02:04:32 +0000

    Returns list of links instead of set

--- src/main/java/net/brishty/sitemap/generator/web/SitemapToSitemapResponseDtoConverter.java
+++ src/main/java/net/brishty/sitemap/generator/web/SitemapToSitemapResponseDtoConverter.java
@@ -4,14 +4,23 @@ import net.brishty.sitemap.generator.service.domain.Sitemap;
 import net.brishty.sitemap.generator.web.domain.SitemapResponseDto;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.Optional;
+import java.util.Set;
+
+import static java.util.List.copyOf;
 
 @Component
 public class SitemapToSitemapResponseDtoConverter {
 
     public SitemapResponseDto convert(Sitemap sitemap) {
+
+        Set<String> links = Optional.of(sitemap)
+                .map(Sitemap::getLinks)
+                .orElse(Collections.emptySet());
+
         return SitemapResponseDto.builder()
-                .links(Optional.of(sitemap).map(Sitemap::getLinks).orElse(null))
+                .links(copyOf(links))
                 .build();
     }
 }
--- src/main/java/net/brishty/sitemap/generator/web/domain/SitemapResponseDto.java
+++ src/main/java/net/brishty/sitemap/generator/web/domain/SitemapResponseDto.java
@@ -6,7 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.Set;
+import java.util.List;
 
 @Getter
 @Builder
@@ -15,5 +15,5 @@ import java.util.Set;
 public class SitemapResponseDto {
 
     @JsonProperty("links")
-    Set<String> links;
+    List<String> links;
 }
